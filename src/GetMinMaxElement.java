05964f633901bb291f1532a2fc7411672f5cc65e
Andrei Railean <andreirailian91@gmai.com>
2022-11-26 19:01:50 +0200

    Algoritms .MergeSort Arrays.

---
+++ src/GetMinMaxElement.java
@@ -0,0 +1,33 @@
+public class GetMinMaxElement {
+    static int getMaxElement(int[] arr, int index) {
+        int max;
+        int length = arr.length;
+
+        if (length > index) {
+            max = getMaxElement(arr, index + 1);
+            if (arr[index] > max)
+                return arr[index];
+            else
+                return max;
+
+        } else {
+            return arr[index - 1];
+        }
+    }
+
+    static int getMinElement(int[] arr, int index) {
+        int min;
+        int length = arr.length;
+
+        if (length > index) {
+            min = getMinElement(arr, index + 1);
+            if (arr[index] < min)
+                return arr[index];
+            else
+                return min;
+
+        } else {
+            return arr[index - 1];
+        }
+    }
+}
